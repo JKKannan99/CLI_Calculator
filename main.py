249ecfbaabfd0fee7f1826fe168fc3a78a787e64
Kannan <jkkannan99@gmail.com>
2025-12-11 18:00:24 +0530

    CLI Calculator

---
+++ main.py
@@ -0,0 +1,38 @@
+
+from src.calc import *
+
+def main():
+    
+    print("Basic Arithmetic Operations")
+    cmd=input('Type y for continue calculation...')  
+    if cmd !='y':
+        print("please type 'y' for proceeding calculation...") 
+    
+
+    else:
+        print("operations are :'add' 'sub' 'mul' 'div'")
+        print("Type exit for close the operations")
+        print("Please provide operations to perform followed with two numbers. Example: add 5 3")
+    
+        while cmd=='y':
+
+            user_input=input("Calc> ").lower().strip().split()
+      
+            command=user_input[0]
+            if command not in ["add", "sub", "mul", "div", "exit"]:
+                print("Enter valuable operators like 'add' 'sub' 'mul' 'div' ")
+                quit()
+
+            if command == "exit" :
+                print("calculator closed... Thanks for using!")
+                break
+            try:
+                num1 = user_input[1]
+                num2 = user_input[2]
+                calculator(command,num1,num2)
+            except:
+                print("Error: Only numbers are allowed.")
+                return
+
+if __name__ == "__main__":
+    main()
---
+++ src/calc.py
@@ -0,0 +1,19 @@
+def calculator(command,num1,num2):
+ 
+    x=float(num1)
+    y=float(num2)
+
+    if command=='add':
+        print("Addition Result:",x + y)
+    elif command=='sub':
+        print("Subtraction Result:",x - y)
+    elif command=='mul':
+        print("Multiplication Result:",x * y)
+    elif command=='div':
+        if y==0:
+            print('cannot divided by zero')
+        else:
+            print("Division Result:",x / y)
+    
+    else:
+        print("unknown command")
