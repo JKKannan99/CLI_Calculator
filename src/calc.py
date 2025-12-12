4e356898c50c479387d16737f76be8700fe071c9
JKKannan99 <jkkannan99@gmail.com>
2025-12-12 13:59:40 +0530

    Update calc.py

--- src/calc.py
+++ src/calc.py
@@ -1,19 +1,22 @@
-def calculator(command,num1,num2):
+# def calculator(command,num1,num2):
  
-    x=float(num1)
-    y=float(num2)
+#     x=float(num1)
+#     y=float(num2)
 
-    if command=='add':
-        print("Addition Result:",x + y)
-    elif command=='sub':
-        print("Subtraction Result:",x - y)
-    elif command=='mul':
-        print("Multiplication Result:",x * y)
-    elif command=='div':
-        if y==0:
-            print('cannot divided by zero')
-        else:
-            print("Division Result:",x / y)
+#     if command=='add':
+#         print("Addition Result:",x + y)
+#     elif command=='sub':
+#         print("Subtraction Result:",x - y)
+#     elif command=='mul':
+#         print("Multiplication Result:",x * y)
+#     elif command=='div':
+#         if y==0:
+#             print('cannot divided by zero')
+#         else:
+#             print("Division Result:",x / y)
     
-    else:
-        print("unknown command")
+#     else:
+#         print("unknown command")
+
+#*************************************************************************************************************
+
